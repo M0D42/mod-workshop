b986c5b79aaef68b1d4e1e15ed0869317e89fded
M0D <yayagarcia768@gmail.com>
2025-06-02 15:10:21 -0600

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/m0d-workshop/', // <-- This must match your repo name!
   plugins: [react()],
-  base: '/m0d-workshop/', // <-- Add this line for GitHub Pages
-})
+});
