5839cdc329d41bd145a0d31d0d53e519671d1bab
M0D <yayagarcia768@gmail.com>
2025-06-02 15:35:31 -0600

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/m0d-workshop/', // <-- This must match your repo name!
+  base: '/m0d-workshop/', // <-- must match your repo name!
   plugins: [react()],
 });
