7973559c98e4fda6b1f04b5608a76b18257694e5
M0D <yayagarcia768@gmail.com>
2025-06-02 18:34:15 -0600

    Initial commit

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -5,9 +5,9 @@ function Navbar() {
     return (
         <nav className="navbar">
             {/* Your navbar content here */}
-        <a href="/mod-workshop/#/home">Home</a>
-        <a href="/mod-workshop/#/blog">blog</a>
-        <a href="#"></a>
+        <a href="#/home">Home</a>
+        <a href="#/blog">blog</a>
+        
             
         </nav>
     );
--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/mod-workshop/', // <-- Add this line, use your repo name!
   plugins: [react()],
 });
